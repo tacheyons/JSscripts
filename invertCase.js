2b1426bbe705809f1e96670abd7162c7831c2edd
Govindaraj <balaji.it2008@gmail.com>
2025-06-04 05:04:13 -0500

    Create invertCase.js

---
+++ invertCase.js
@@ -0,0 +1,15 @@
+let st = 'The Quick Brown Foxvar';
+
+let r = st.split('');
+console.log(st);
+const temp = [];
+const x = r.forEach((item, index) => {
+  if(item !== '') {
+    if(item === st[index].toUpperCase()){
+      temp.push(st[index].toLowerCase());
+    } else {
+      temp.push(st[index].toUpperCase());
+    }
+  }
+});
+console.log(temp.join(''));
