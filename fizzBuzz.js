aa0d70bb167569b1ab319ec1388a896f28f66933
Govindaraj <balaji.it2008@gmail.com>
2026-06-14 12:54:46 -0500

    fizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,31 @@
+var fizzBuzz = function(n) {
+    var answer = [];
+    for(var i=1; i<=n; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            answer.push("FizzBuzz")
+        } else if(i%3 === 0) {
+            answer.push("Fizz");
+        } else if(i%5 === 0) {
+            answer.push("Buzz");
+        } else {
+            answer.push((i).toString());
+        }
+    }
+
+    return answer;
+};
+
+/*
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+*/
